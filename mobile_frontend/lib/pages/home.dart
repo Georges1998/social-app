018ead82730734564c53dedd27aa2f0c26f36f08
George Kandalaft <george.kandalaft@mail.mcgill.ca>
2020-04-01 04:13:29 -0400

    Home Page

--- mobile_frontend/lib/pages/home.dart
+++ mobile_frontend/lib/pages/home.dart
@@ -6,8 +6,47 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  bool isAuth = false;
+
+  Widget buildAuthScreen() {
+    return Text('Authenticated');
+  }
+
+  Scaffold buildUnAuthScreen() {
+    return Scaffold(
+        body: Container(
+      decoration: BoxDecoration(
+          gradient: LinearGradient(
+              begin: Alignment.topRight,
+              end: Alignment.bottomLeft,
+              colors: [Colors.teal, Colors.pink])),
+              alignment: Alignment.center,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Text(
+            "Social Distancing",
+            style: TextStyle(fontSize: 80.0, color: Colors.white, fontFamily: 'Signatra'),
+          ),
+          GestureDetector(
+            child: Container(
+              width: 260,
+              height: 60,
+              decoration: BoxDecoration(
+                  image: DecorationImage(
+                      image:
+                          AssetImage("assets/images/google_signin_button.png"),
+                      fit: BoxFit.cover)),
+            ),
+          )
+        ],
+      ),
+    ));
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Text("Home");
+    return isAuth ? buildAuthScreen() : buildUnAuthScreen();
   }
 }
