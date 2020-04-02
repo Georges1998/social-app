e5cef2c0e70db5e046ee41d63ae572f6016634a6
George Kandalaft <george.kandalaft@mail.mcgill.ca>
2020-04-02 15:19:28 -0400

    Connecting firebase database

--- mobile_frontend/lib/pages/timeline.dart
+++ mobile_frontend/lib/pages/timeline.dart
@@ -1,6 +1,9 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:mobile_frontend/widgets/progress.dart';
 import '../widgets/header.dart';
+
+final usersRef = Firestore.instance.collection("users");
 
 class Timeline extends StatefulWidget {
   @override
@@ -9,10 +12,23 @@ class Timeline extends StatefulWidget {
 
 class _TimelineState extends State<Timeline> {
   @override
+  void initState() {
+    getUsers();
+    super.initState();
+  }
+
+  getUsers() {
+    usersRef.getDocuments().then((QuerySnapshot snapshot) {
+      snapshot.documents.forEach((DocumentSnapshot doc) {
+        print(doc.data);
+      });
+    });
+  }
+
+  @override
   Widget build(context) {
     return Scaffold(
       appBar: header(isAppTitle: true, text: "RichieRich"),
-      body: linearProgress(),
     );
   }
 }
