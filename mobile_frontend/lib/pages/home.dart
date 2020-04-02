bfbefb3e91f0f5948f3e735bc9d2b604b7ac351f
George Kandalaft <george.kandalaft@mail.mcgill.ca>
2020-04-02 12:37:26 -0400

    Login/Logout

--- mobile_frontend/lib/pages/home.dart
+++ mobile_frontend/lib/pages/home.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
 final googleSignIn = GoogleSignIn();
+
 class Home extends StatefulWidget {
   @override
   _HomeState createState() => _HomeState();
@@ -11,28 +12,49 @@ class _HomeState extends State<Home> {
   bool isAuth = false;
 
   @override
-  void initState() { 
+  void initState() {
     super.initState();
-    googleSignIn.onCurrentUserChanged.listen((account){
-      if(account!=null){
-        setState(() {
-          isAuth = true;
-        });
-        print("account is authenticated: $account");
-      }else{
-        setState(() {
-          isAuth = false;
-        });
-      }
+    googleSignIn.onCurrentUserChanged.listen((account) {
+      handleSignIn(account);
+    }, onError: (err) {
+      print("Error sigining in: $err");
+    });
+    // Reauthenticate users when app is open
+    googleSignIn.signInSilently(suppressErrors: false).then((account) {
+      handleSignIn(account);
+    }).catchError((err){
+      print("Error sigining in: $err");
     });
   }
-  login(){
+
+  handleSignIn(GoogleSignInAccount account) {
+    if (account != null) {
+      setState(() {
+        isAuth = true;
+      });
+      print("account is authenticated: $account");
+    } else {
+      setState(() {
+        isAuth = false;
+      });
+    }
+  }
+
+  login() {
     googleSignIn.signIn();
   }
 
-  Widget buildAuthScreen() {
-    return Text('Authenticated');
+  logout(){
+    googleSignIn.signOut();
   }
+
+  Widget buildAuthScreen() {
+    return RaisedButton(
+      child: Text("Logout"),
+      onPressed: logout,
+    );
+  }
+
 
   Scaffold buildUnAuthScreen() {
     return Scaffold(
@@ -42,14 +64,15 @@ class _HomeState extends State<Home> {
               begin: Alignment.topRight,
               end: Alignment.bottomLeft,
               colors: [Theme.of(context).accentColor, Colors.blue])),
-              alignment: Alignment.center,
+      alignment: Alignment.center,
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: <Widget>[
           Text(
             "RichieRich",
-            style: TextStyle(fontSize: 65.0, color: Colors.white, fontFamily: 'LuckiestGuy'),
+            style: TextStyle(
+                fontSize: 65.0, color: Colors.white, fontFamily: 'LuckiestGuy'),
           ),
           GestureDetector(
             onTap: login,
