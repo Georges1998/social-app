8c1067c863ab4c8e8d459d0f2cb7d01f7d534062
George Kandalaft <george.kandalaft@mail.mcgill.ca>
2020-04-05 12:40:27 -0400

    Upload Image Screen

--- mobile_frontend/lib/pages/upload.dart
+++ mobile_frontend/lib/pages/upload.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class Upload extends StatefulWidget {
   @override
@@ -6,8 +9,79 @@ class Upload extends StatefulWidget {
 }
 
 class _UploadState extends State<Upload> {
+  File file;
+  handleTakePhoto() async {
+    Navigator.pop(context);
+    File file = await ImagePicker.pickImage(
+        source: ImageSource.camera, maxHeight: 675, maxWidth: 960);
+    this.file = file;
+  }
+
+  handleChooseImage() async{
+    Navigator.pop(context);
+    File file = await ImagePicker.pickImage(
+        source: ImageSource.gallery);
+    this.file = file;
+  }
+
+  selectImage(parentContext) {
+    return showDialog(
+        context: parentContext,
+        builder: (context) {
+          return SimpleDialog(
+            title: Text("Create Post"),
+            children: <Widget>[
+              SimpleDialogOption(
+                child: Text("Photo with Camera"),
+                onPressed: handleTakePhoto,
+              ),
+              SimpleDialogOption(
+                child: Text("Image from the Gallery"),
+                onPressed: handleChooseImage,
+              ),
+              SimpleDialogOption(
+                child: Text("Cancel"),
+                onPressed: () => Navigator.pop(context),
+              ),
+            ],
+          );
+        });
+  }
+
+  Container buildSplashScreen() {
+    return Container(
+      color: Theme.of(context).primaryColor,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Icon(
+            Icons.camera_alt,
+            size: 250.0,
+            color: Theme.of(context).accentColor,
+          ),
+          Padding(
+            padding: EdgeInsets.only(top: 20),
+            child: RaisedButton(
+              disabledColor: Theme.of(context).accentColor,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(8.0)),
+              child: Text(
+                "Share Image",
+                style: TextStyle(
+                    color: Theme.of(context).primaryColor,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 30),
+              ),
+              onPressed: () => selectImage(context),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text("Upload"));
+    return buildSplashScreen();
   }
 }
